df1c9e91a20eb4f5fd5f3b9bdc7081137ae842d2
elMathos <mathurin.massias@gmail.com>
2014-10-31 16:32:43 +0100

    Plotting of ellipsis

---
+++ TP2/PlotEllipsis.R
@@ -0,0 +1,28 @@
+library(mvtnorm) # References rmvnorm()
+plot(dat)
+
+source("EM.R")
+
+plotDataEllipsis = function(dat, k, initCentr, levels, diagonalVariance){
+  result = gaussianMixtureEM(dat, k, initCentr, diagonalVariance)
+  plot(dat, asp= 1)
+  n = 20
+  x <- seq(-10, 10, 1/n)
+  y <- seq(-10, 10, 1/n)
+  
+  for (i in 1:k)
+  {
+    center = result$mu[[i]]
+    sigma = as.matrix(result$sigma[[i]])
+      
+    sigma.inv = solve(sigma, matrix(c(1,0,0,1),2,2))
+      
+    ellipse <- function(s,t) {u<-c(s,t)-center; u %*% sigma.inv %*% u / 2}
+    
+    numberPoints = length(x)
+    z <- mapply(ellipse, as.vector(rep(x,numberPoints)), as.vector(outer(rep(0,numberPoints), y, `+`)))
+    contour(x,y,matrix(z,numberPoints,numberPoints), levels=levels, col = terrain.colors(10), add=TRUE)
+  }
+}
+
+plotDataEllipsis(dat, 4, initCentr, (1:9), T)
