eee85107ffa583b03bed7cd9ab3d4e93a42c5e42
elMathos <mathurin.massias@gmail.com>
2014-11-01 19:42:57 +0100

    Color change

--- TP2/KMeans.R
+++ TP2/KMeans.R
@@ -56,7 +56,7 @@ KMeans = function(tab, K, seed)
   plot(tab, col = ifelse(clusters == 1, "red",
                          ifelse(clusters == 2, "blue",
                                 ifelse(clusters == 3, "green",
-                                       ifelse(clusters == 4, "yellow", "black")))),
+                                       ifelse(clusters == 4, "orange", "black")))),
        pch=16,cex=0.5)
   points(centroids, pch=13, cex =2)
   
