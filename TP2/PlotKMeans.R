c8b6d912ff434fa489be31031958a2b55bc9f4a0
Nick-Ol <clement.nicolle@student.ecp.fr>
2014-11-02 20:58:42 +0100

    Separate clusters and distortion plottings

--- TP2/PlotKMeans.R
+++ TP2/PlotKMeans.R
@@ -1,15 +1,19 @@
-plotKMeans = function(data, KMeansResults)
+plotClusters = function(data, KMeansResults)
 {
   centroids = KMeansResults$centroids
   clusters = KMeansResults$clusters
-  distortions = KMeansResults$distortions
   
   plot(tab, col = ifelse(clusters == 1, "red",
                          ifelse(clusters == 2, "blue",
                                 ifelse(clusters == 3, "green",
                                        ifelse(clusters == 4, "orange", "black")))),
        pch=16,cex=0.5)
-  points(centroids, pch=13, cex =2)
   
+  points(centroids, pch=13, cex =2)
+}
+
+plotDistortion = function(KMeansResults)
+{
+  distortions = KMeansResults$distortions
   plot(distortions)
 }
