cdc0e2f4d7a43330809e8b17c23553e4c87b283d
elMathos <mathurin.massias@gmail.com>
2014-11-12 07:26:57 +0100

    Typo and sourcing in Main.R

--- TP2/Main.R
+++ TP2/Main.R
@@ -1,6 +1,8 @@
 source("KMeans.R")
 source("EM.R")
 source("PlotEllipsis.R")
+source("PlotKMeans.R")
+source("EMllh.R")
 
 ## PARAMETERS
 dat = read.table("Data/EMGaussian.data", header=F, sep=' ')
@@ -9,14 +11,15 @@ levels = (1:9) #which level lines do you want to be plotted?
 ##
 
 
-init = KMeans(dat, K, 1)
+init = KMeans(dat, K, 45)
 initCentr = init$centroids
+plotClusters(dat, init)
 resultsEMDiag = gaussianMixtureEM(dat, K, initCentr, T)
 resultsEMNotDiag = gaussianMixtureEM(dat, K, initCentr, F)
 
 par(mfrow=c(1,1))
 plotDataEllipsis(dat, resultsEMDiag)
-plotDataEllipsis(dat, resultsEMNotdiag)
+plotDataEllipsis(dat, resultsEMNotDiag)
 
 datTest = read.table("Data/EMGaussian.test", header=F, sep=' ')
 cat('llh sur le dataset Test avec covariances diagonales = ', EMllh(datTest, resultsEMDiag) ,'\n')
